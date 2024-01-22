d45dcd203e36351d4c50f9fe654df7c6e1a561fa
chamarasab <chamara.sab@outlook.com>
2024-01-22 12:21:36 +0530

    add belongs to return type

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -4,16 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Attendee extends Model
 {
     use HasFactory;
 
-    public function user() {
+    public function user() :BelongsTo {
         return $this->belongsTo(User::class);
     }
 
-    public function event() {
+    public function event() :BelongsTo {
         return $this->belongsTo(Event::class);
     }
 }
