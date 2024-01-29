25b271c586e7b38320becdde54012f66fe2195bf
chamarasab <chamara.sab@outlook.com>
2024-01-29 13:50:13 +0530

    create update retrieve tested using postman

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\Api\AttendeeController;
-use App\Http\Controllers\Api\EventController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::apiResource('events',EventController::class);
-Route::apiResource('events.attendees',AttendeeController::class)->scoped(['attendee'=>'event']);
